a0dc845501cabfd6adda6fa1095860e8340de4fa
Olle Jonsson <olle.jonsson@auctionet.com>
2020-04-30 15:25:43 +0200

    Spec: use expect/to matchers

--- spec/collector_spec.rb
+++ spec/collector_spec.rb
@@ -6,20 +6,20 @@ describe Dboard::Collector, "register_source" do
   end
 
   it "can register a source" do
-    new_relic = mock
-    new_relic.stub!(:update_interval).and_return(5)
+    new_relic = double
+    allow(new_relic).to receive(:update_interval).and_return(5)
     Dboard::Collector.register_source :new_relic, new_relic
-    Dboard::Collector.instance.sources.should == { :new_relic => new_relic }
+    expect(Dboard::Collector.instance.sources).to eq({ :new_relic => new_relic })
   end
 
   it "can register an after update callback" do
-    new_relic = mock
-    new_relic.stub!(:fetch).and_return({ :db => "100%" })
-    callback = mock
+    new_relic = double
+    allow(new_relic).to receive(:fetch).and_return({ :db => "100%" })
+    callback = double
     Dboard::Collector.register_after_update_callback callback
 
-    callback.should_receive(:call)
-    Dboard::Publisher.stub!(:publish)
+    expect(callback).to receive(:call)
+    allow(Dboard::Publisher).to receive(:publish)
     Dboard::Collector.instance.update_source(:new_relic, new_relic)
 
     # since it is a singleton, and this callbacks leaks into the other tests
@@ -27,14 +27,14 @@ describe Dboard::Collector, "register_source" do
   end
 
   it "can register an error callback" do
-    new_relic = mock
+    new_relic = double
     error = RuntimeError.new("error")
-    new_relic.stub!(:fetch).and_raise(error)
-    callback = mock
+    allow(new_relic).to receive(:fetch).and_raise(error)
+    callback = double
     Dboard::Collector.register_error_callback callback
 
-    callback.should_receive(:call).with(error)
-    Dboard::Publisher.stub!(:publish)
+    expect(callback).to receive(:call).with(error)
+    allow(Dboard::Publisher).to receive(:publish)
     Dboard::Collector.instance.update_source(:new_relic, new_relic)
 
     # since it is a singleton, and this callbacks leaks into the other tests
@@ -48,16 +48,16 @@ describe Dboard::Collector, "update_source" do
   end
 
   it "should collect and publish data from sources" do
-    new_relic = mock
-    new_relic.stub!(:fetch).and_return({ :db => "100%" })
-    Dboard::Publisher.should_receive(:publish).with(:new_relic, { :db => "100%" })
+    new_relic = double
+    allow(new_relic).to receive(:fetch).and_return({ :db => "100%" })
+    expect(Dboard::Publisher).to receive(:publish).with(:new_relic, { :db => "100%" })
     Dboard::Collector.instance.update_source(:new_relic, new_relic)
   end
 
   it "should print out debugging info" do
-    new_relic = mock
-    new_relic.stub!(:fetch).and_raise(Exception.new("some error"))
-    Dboard::Collector.instance.should_receive(:puts).twice
+    new_relic = double
+    allow(new_relic).to receive(:fetch).and_raise(Exception.new("some error"))
+    expect(Dboard::Collector.instance).to receive(:puts).twice
     Dboard::Collector.instance.update_source(:new_relic, new_relic)
   end
 end
