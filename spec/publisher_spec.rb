1db1ddccc6c537046f6288d7bbc248af9c067b89
Peter Wall <peter@auctionet.com>
2021-07-30 16:28:55 +0200

    Whitespace

--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 
 describe "Publisher", "publish" do
-  
   it "should send data to the dashboard server" do
     expect(Dboard::Api::Client).to receive(:post).with("/sources/new_relic", :body => { :data => { :db => "80%" }.to_json }, :timeout => 10000)
     Dboard::Publisher.publish(:new_relic, { :db => "80%" })
@@ -12,5 +11,4 @@ describe "Publisher", "publish" do
     expect(Dboard::Publisher).to receive(:puts).with("SocketError: failed to connect")
     Dboard::Publisher.publish(:new_relic, {})
   end
-
 end
